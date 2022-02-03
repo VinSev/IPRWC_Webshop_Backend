a7c9f967e6f2dee7c16bd2aa1144e6a936093241
VinSe <vincentmcmaps@gmail.com>
2022-02-03 11:32:40 +0100

    initial commit

---
+++ src/main/java/nl/hsleiden/IPRWC_Webshop_Backend/IprwcWebshopBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.hsleiden.IPRWC_Webshop_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IprwcWebshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IprwcWebshopBackendApplication.class, args);
+	}
+
+}
