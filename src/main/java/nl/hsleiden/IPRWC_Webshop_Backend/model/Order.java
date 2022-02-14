c015313d48b17c7653762aa30b1cfc5209368856
VinSe <vincentmcmaps@gmail.com>
2022-02-14 22:58:02 +0100

    Changed String to Long

--- src/main/java/nl/hsleiden/IPRWC_Webshop_Backend/model/Order.java
+++ src/main/java/nl/hsleiden/IPRWC_Webshop_Backend/model/Order.java
@@ -12,7 +12,7 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @NotBlank
-    private String userId;
+    private Long userId;
     @NotBlank
     private Timestamp dateLastUpdated;
     @NotBlank
@@ -34,11 +34,11 @@ public class Order {
         this.id = id;
     }
 
-    public String getUserId() {
+    public Long getUserId() {
         return userId;
     }
 
-    public void setUserId(String userId) {
+    public void setUserId(Long userId) {
         this.userId = userId;
     }
 
