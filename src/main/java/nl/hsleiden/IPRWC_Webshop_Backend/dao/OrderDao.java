df1b64d31d1db0b47ca552208252dd2f5215a657
VinSe <vincentmcmaps@gmail.com>
2022-02-21 13:53:30 +0100

    Cleanup

--- src/main/java/nl/hsleiden/IPRWC_Webshop_Backend/dao/OrderDao.java
+++ src/main/java/nl/hsleiden/IPRWC_Webshop_Backend/dao/OrderDao.java
@@ -38,7 +38,6 @@ public class OrderDao implements Dao<Order, Long> {
     @Override
     @Transactional
     public Order create(Order order) {
-        System.out.println(order.getUserId());
         this.orderRepository.saveAndFlush(order);
         for (Product product : order.getProducts()) {
             this.orderItemRepository.save(new OrderItem(order.getId(), product.getId(), product.getAmount()));
